0516ff515a903d4cc0039747bc668795149cedc7
Alessandro Mao <alessandro.mao@edu.unito.it>
2024-01-22 18:55:34 +0100

    Added singleGameService

---
+++ twebReact/src/services/singleGameService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001/single-game';
+
+export const getGameEventsById = (gameId) => {
+    return axios.get(`${BASE_URL}/get-game-events-by-id/${gameId}`);
+};
+
+export const getGameById = (gameId) => {
+    return axios.get(`${BASE_URL}/get-game-by-id/${gameId}`);
+};
+
+
+
